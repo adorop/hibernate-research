76faa814bdb59068864aef6c27474facb3bbbe8b
adorop <adorop@ya.ru>
2017-05-27 18:41:58 +0300

    creating producdt converter

---
+++ src/main/java/adorop/dto/converters/DtoToProductConverter.java
@@ -0,0 +1,33 @@
+package adorop.dto.converters;
+
+import adorop.dao.DAO;
+import adorop.dto.ProductDto;
+import adorop.model.Product;
+import adorop.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DtoToProductConverter implements Converter<ProductDto, Product> {
+    private final DAO<User> userDAO;
+
+    @Autowired
+    public DtoToProductConverter(DAO<User> userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    public Product convert(ProductDto productDto) {
+        Product product = new Product();
+        product.setId(productDto.getId());
+        product.setDescription(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+        product.setOwner(findOwner(productDto));
+        return product;
+    }
+
+    private User findOwner(ProductDto productDto) {
+        return userDAO.find(productDto.getOwnerId());
+    }
+}
