2eb94fb299ed70e7d9df5dbc5b31a5d7f56791b3
adorop <adorop@ya.ru>
2017-05-27 19:55:18 +0300

    custom validation in service

--- src/main/java/adorop/service/ProductServiceImpl.java
+++ src/main/java/adorop/service/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package adorop.service;
 import adorop.dao.DAO;
 import adorop.model.Product;
 import adorop.service.dto.ProductDto;
+import adorop.service.validators.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductServiceImpl implements ProductService {
     private final DAO<Product> productDAO;
     private final Converter<ProductDto, Product> converter;
+    private final Validator<ProductDto> validator;
 
     @Autowired
-    public ProductServiceImpl(DAO<Product> productDAO, Converter<ProductDto, Product> converter) {
+    public ProductServiceImpl(DAO<Product> productDAO,
+                              Converter<ProductDto, Product> converter,
+                              Validator<ProductDto> validator) {
         this.productDAO = productDAO;
         this.converter = converter;
+        this.validator = validator;
     }
 
     @Override
     public Product save(ProductDto productDto) {
+        validator.validate(productDto);
         return productDAO.save(converter.convert(productDto));
     }
 }
