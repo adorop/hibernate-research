60c62742997f95babc63008c8153185686591a65
adorop <adorop@ya.ru>
2017-05-28 00:12:00 +0300

    mapping assocation to products

--- src/main/java/adorop/model/User.java
+++ src/main/java/adorop/model/User.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "USER")
@@ -15,4 +17,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.PERSIST)
+    private Set<Product> products = new HashSet<>();
 }
