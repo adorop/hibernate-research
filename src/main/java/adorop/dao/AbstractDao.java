fcae35d27a605a4faa21b43ccca7837b9c3a21ab
adorop <adorop@ya.ru>
2017-05-27 17:24:31 +0300

    creating abstract dao

---
+++ src/main/java/adorop/dao/AbstractDao.java
@@ -0,0 +1,45 @@
+package adorop.dao;
+
+import adorop.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public abstract class AbstractDao<T> implements DAO<T> {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public List<T> findAll() {
+        CriteriaQuery<T> query = entityManager.getCriteriaBuilder().createQuery(persistedClass());
+        query.select(query.from(persistedClass()));
+        return entityManager.createQuery(query)
+                .getResultList();
+    }
+
+    @Override
+    public T save(T entity) {
+        return null;
+    }
+
+    @Override
+    public T update(T entity) {
+        return null;
+    }
+
+    @Override
+    public T find(Long id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    protected abstract Class<T> persistedClass();
+}
