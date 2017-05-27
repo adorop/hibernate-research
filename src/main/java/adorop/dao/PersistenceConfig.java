40587393c027ccf26c3569e9f1e669297219cdf9
adorop <adorop@ya.ru>
2017-05-27 17:06:44 +0300

    configuring jpa

--- src/main/java/adorop/dao/PersistenceConfig.java
+++ src/main/java/adorop/dao/PersistenceConfig.java
@@ -1,10 +1,52 @@
 package adorop.dao;
 
+import com.jolbox.bonecp.BoneCPDataSource;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @ComponentScan("adorop.dao")
+@EnableTransactionManagement
 public class PersistenceConfig {
+    @Bean
+    DataSource dataSource() {
+        BoneCPDataSource boneCPDataSource = new BoneCPDataSource();
+        boneCPDataSource.setDriverClass("com.mysql.jdbc.Driver");
+        boneCPDataSource.setUsername("root");
+        boneCPDataSource.setPassword("1234");
+        boneCPDataSource.setJdbcUrl("jdbc:mysql://localhost:3306/user_products_hibernate");
+        return boneCPDataSource;
+    }
+    @Bean
+    LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setPackagesToScan("adorop.model");
+        entityManagerFactoryBean.setPersistenceProvider(new HibernatePersistenceProvider());
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setJpaPropertyMap(getProperties());
+        return entityManagerFactoryBean;
+    }
 
+    private Map<String, String> getProperties() {
+        HashMap<String, String> properties = new HashMap<>();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.format_sql", "true");
+        return properties;
+    }
+
+    @Bean
+    PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager(entityManagerFactoryBean().getObject());
+    }
 }
