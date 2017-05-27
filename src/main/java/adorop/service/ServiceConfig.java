b273e056b3bb6b453207e2af9db6f7d6fb2e2b21
adorop <adorop@ya.ru>
2017-05-27 17:48:24 +0300

    import dao to service

--- src/main/java/adorop/service/ServiceConfig.java
+++ src/main/java/adorop/service/ServiceConfig.java
@@ -1,8 +1,10 @@
 package adorop.service;
 
+import adorop.dao.PersistenceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -12,6 +14,7 @@ import javax.persistence.EntityManagerFactory;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan("adorop.service")
+@Import(PersistenceConfig.class)
 public class ServiceConfig {
     @Bean
     PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
