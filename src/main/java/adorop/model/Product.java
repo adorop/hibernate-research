b91295bc2889cf67ae71eff4905c900749fcf199
adorop <adorop@ya.ru>
2017-05-27 17:50:55 +0300

    describing id of product

--- src/main/java/adorop/model/Product.java
+++ src/main/java/adorop/model/Product.java
@@ -10,6 +10,8 @@ import java.math.BigDecimal;
 @Table(name = "PRODUCT")
 @Getter @Setter
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
     private BigDecimal price;
