dcb8830e65cba08b802481735ae98bab75bf1b56
adorop <adorop@ya.ru>
2017-05-27 17:50:06 +0300

    making user service transactional

--- src/main/java/adorop/service/UserServiceImpl.java
+++ src/main/java/adorop/service/UserServiceImpl.java
@@ -4,10 +4,12 @@ import adorop.dao.DAO;
 import adorop.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
     private final DAO<User> userDAO;
 
