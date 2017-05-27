e1ab76ff85a531d82195f808d1b5dba9283df6ba
adorop <adorop@ya.ru>
2017-05-27 18:36:25 +0300

    creating product dto

---
+++ src/main/java/adorop/dto/ProductDto.java
@@ -0,0 +1,13 @@
+package adorop.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductDto {
+    private Long id;
+    private String description;
+    private BigDecimal price;
+    private Long ownerId;
+}
