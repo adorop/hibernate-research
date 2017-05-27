2ace5d7eca5e8502a802737895040d3d41fc9238
adorop <adorop@ya.ru>
2017-05-27 19:46:39 +0300

    response status 400

--- src/main/java/adorop/web/ControllersExceptionHandler.java
+++ src/main/java/adorop/web/ControllersExceptionHandler.java
@@ -2,12 +2,15 @@ package adorop.web;
 
 import adorop.service.BadRequestException;
 import adorop.service.dto.BadRequestDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllersExceptionHandler {
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public BadRequestDto handle(BadRequestException e) {
         return new BadRequestDto(e.getMessage());
     }
