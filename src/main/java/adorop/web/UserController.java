ccdc8ef9f96c9204643106fa0cb434fa4df213be
adorop <adorop@ya.ru>
2017-05-28 00:19:51 +0300

    creating user controller

---
+++ src/main/java/adorop/web/UserController.java
@@ -0,0 +1,24 @@
+package adorop.web;
+
+import adorop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
+}
